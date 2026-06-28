d36cd2b6798e0e0068f2e704d30db72306463b0e
Fireman <firemandeveloper25@gmail.com>
2026-06-29 00:08:24 +0330

    new domain

--- Firenet/app/src/main/java/com/v2ray/ang/net/DomainFallback.kt
+++ Firenet/app/src/main/java/com/v2ray/ang/net/DomainFallback.kt
@@ -18,8 +18,7 @@ object DomainFallback {
     /** لیست دامین‌ها. ترتیب اهمیت دارد. مورد اول ارجح است. */
     private val domains = CopyOnWriteArrayList<String>(
         listOf(
-            "https://fallback1.soft99.sbs:443",
-            "https://fallback1.soft99.sbs:2053",
+            "https://api.firenetapp.ir",
             "https://firenet.mapmah2025.workers.dev"
         )
     )
